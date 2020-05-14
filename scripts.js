17fbf49bf7dc0f3c2fa9642c2cbd9501b075a649
Rebecca Hutubessy <45396621+hutuber001@users.noreply.github.com>
2020-05-14 12:02:18 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function schroef {
+    var schroeflinks1 = document.querySelectorAll('.schroeflinks1');
+    var schroeflinks2 = document.querySelectorAll('.schroeflinks2');
+
+    while schroeflinks1
+}
